26fdf5ee292825700bfcc5f7b90fd9767607ea5d
greencap <vladnicut@yahoo.com>
2018-08-14 19:56:13 +0100

    Animation done top down + fade out at the end. Have to set life to remove completely not just fade out. PEACE :X

--- app/src/main/java/com/updog/greencap/updogschoolalpha/MiniGameStart.java
+++ app/src/main/java/com/updog/greencap/updogschoolalpha/MiniGameStart.java
@@ -2,10 +2,11 @@ package com.updog.greencap.updogschoolalpha;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
 import android.view.animation.Animation;
+import android.view.animation.Animation.AnimationListener;
 import android.view.animation.AnimationUtils;
 import android.widget.TextView;
-
 import java.util.HashMap;
 
 public class MiniGameStart extends AppCompatActivity {
@@ -14,7 +15,7 @@ public class MiniGameStart extends AppCompatActivity {
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_mini_game_start);
-    TextView textView = findViewById(R.id.minigame);
+    final TextView textView = findViewById(R.id.minigame);
 
     HashMap<String, String> map = new HashMap<>();
 
@@ -31,13 +32,30 @@ public class MiniGameStart extends AppCompatActivity {
             R.anim.translate);
 
     textView.startAnimation(animTranslate);
+    animTranslate.setFillAfter(true);
 
+    animTranslate.setAnimationListener(new AnimationListener() {
+      @Override
+      public void onAnimationStart(Animation animation) {
 
-    if(textView.getY() == 1700) {
+      }
 
-      Animation animFadeOut = AnimationUtils.loadAnimation(getApplicationContext(),
-              R.anim.fade_out);
-      textView.startAnimation(animFadeOut);
-    }
+      @Override
+      public void onAnimationRepeat(Animation animation) {
+
+      }
+
+      @Override
+      public void onAnimationEnd(Animation animation) {
+        Animation animFadeOut = AnimationUtils.loadAnimation(getApplicationContext(),
+                R.anim.fade_out);
+        textView.startAnimation(animFadeOut);
+      }
+    });
+
+    DisplayMetrics displayMetrics = new DisplayMetrics();
+    getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
+    int height = displayMetrics.heightPixels;
+
   }
 }
